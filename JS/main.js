5cb3a3b432d2ae733d64a1a117fce4f0bc4d80f8
Papytuc1 <kevinsanmartino@gmail.com>
2019-02-04 17:03:31 +0100

    modif du js

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,11 @@
-console.log("salut");
+/* console.log("salut");
 
 let titre = "titre";
 let realisateur = "Un realisateur";
 let dateDeSortie = "Date de sortie";
 let synopsis = "un synopsis";
 let nbEntrees = 1000;
+
 //creation d'un objet json qui s'appel film normalement en fichier a part
 let filmJson = {
     titre: "titre",
@@ -12,7 +13,7 @@ let filmJson = {
     dateDeSortie: "Date de sortie",
     synopsis: "un synopsis",
     nbEntrees: 1000
-};
+}; */
 
 let films = [{
         titre: "titre 1",
@@ -26,13 +27,31 @@ let films = [{
         realisateur: "realisateur 2",
         dateDeSortie: "Date de sortie 2",
         synopsis: "un synopsis 2",
-        nbEntrees: 1000
+        nbEntrees: 2000
     },
     {
-        titre:"titre 3",
-    realisateur: "realisateur 3",
-    dateDeSortie:"Date de sortie 3",
-    synopsis: "un synopsis 3",
-    nbEntrees : 1000
+        titre: "titre 3",
+        realisateur: "realisateur 3",
+        dateDeSortie: "Date de sortie 3",
+        synopsis: "un synopsis 3",
+        nbEntrees: 3000
     }
 ];
+let filmFiltre = films.filter(films => films.nbEntrees>=1500);
+console.log(filmFiltre);
+
+var e = filmFiltre.reduce(function(accumulateur, valeurCourante){
+return  accumulateur.nbEntrees+valeurCourante.nbEntrees;
+}); 
+console.log("je suis la"+e);
+/* const reducer = (accumulator, currentValue) => accumulator + currentValue.nbEntrees;
+console.log(films.reduce(reducer,0)); */
+for(let i =0; i<films.length;i++){
+    console.log(films[i].titre);
+}
+var test=0;
+for (var i = 0; i<films.length;i++){
+    test += films[i].nbEntrees;
+}
+console.log(test);
+document.querySelector(".film").innerHTML+=films[0].titre;
