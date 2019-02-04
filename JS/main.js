65332d107b622cc3656e78181515dae5c5382bb1
Papytuc1 <kevinsanmartino@gmail.com>
2019-02-04 12:21:48 +0100

    ajout du projet film

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+console.log("salut");
+
+let titre = "titre";
+let realisateur = "Un realisateur";
+let dateDeSortie = "Date de sortie";
+let synopsis = "un synopsis";
+let nbEntrees = 1000;
+//creation d'un objet json qui s'appel film normalement en fichier a part
+let filmJson = {
+    titre: "titre",
+    realisateur: "realisateur",
+    dateDeSortie: "Date de sortie",
+    synopsis: "un synopsis",
+    nbEntrees: 1000
+};
+
+let films = [{
+        titre: "titre 1",
+        realisateur: "realisateur 1",
+        dateDeSortie: "Date de sortie 1",
+        synopsis: "un synopsis1",
+        nbEntrees: 1000
+    },
+    {
+        titre: "titre 2",
+        realisateur: "realisateur 2",
+        dateDeSortie: "Date de sortie 2",
+        synopsis: "un synopsis 2",
+        nbEntrees: 1000
+    },
+    {
+        titre:"titre 3",
+    realisateur: "realisateur 3",
+    dateDeSortie:"Date de sortie 3",
+    synopsis: "un synopsis 3",
+    nbEntrees : 1000
+    }
+];
