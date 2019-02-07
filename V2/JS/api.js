9c6b53fd539bb9e910dc9bdb3b46ce87ec517a1d
Papytuc1 <papytuc@hotmail.fr>
2019-02-07 20:48:24 +0100

    ajout de contenu

--- V2/JS/api.js
+++ V2/JS/api.js
@@ -20,53 +20,52 @@ let article;
 let para;
 let img;
 let div;
-let idDeFilm=[];
+let idDeFilm = [];
+
 function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {      
-      if (this.readyState == 4 && this.status == 200) {    
-          let e = JSON.parse(this.responseText);
-          console.log(e);
-          e.results.forEach(function(element,index){
-            idDeFilm[index] = element.id;           
-            article = document.createElement('article');
-              //lien
-              let lien = document.createElement('a');
-              lien.href=`unFilm.html?idDeFilm=${element.id}`;
-             // texte
-             para = document.createElement('p');
-             para.textContent= element.title;
-             para.classList='titre';
-             desc = document.createElement('p');
-             desc.className='description';
-             desc.textContent= element.overview;
-             // image
-             img = document.createElement('img');
-             img.src="https://image.tmdb.org/t/p/w200"+element.poster_path;
-             img.className='uneImage';          
-             article.onmouseenter = function(){
-                this.querySelector('img').classList='animate-opacity-img';
-                this.querySelector('.description').classList ='description animate-opacity-text';
-                this.querySelector('p').classList ='titre animate-opacity-text';            
-            };
-            article.onmouseleave=function(){
-                this.querySelector('img').classList='animate-opacity-text';
-                this.querySelector('.description').classList ='description animate-opacity-img';
-                this.querySelector('p').classList ='titre animate-opacity-img';   
-            };         
-            article.appendChild(img);
-            article.className ='taille';                     
-            article.appendChild(para);
-            article.appendChild(desc);
-            lien.appendChild(article);
-            document.querySelector('#film').appendChild(lien);
-        });
-      };
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let e = JSON.parse(this.responseText);
+      console.log(e);
+      e.results.forEach(function (element, index) {
+        idDeFilm[index] = element.id;
+        article = document.createElement('article');
+        //lien
+        let lien = document.createElement('a');
+        lien.href = `unFilm.html?idDeFilm=${element.id}`;
+        // texte
+        para = document.createElement('p');
+        para.textContent = element.title;
+        para.classList = 'titre';
+        desc = document.createElement('p');
+        desc.className = 'description';
+        desc.textContent = element.overview;
+        // image
+        img = document.createElement('img');
+        img.src = "https://image.tmdb.org/t/p/w200" + element.poster_path;
+        img.className = 'uneImage';5
+        article.onmouseenter = function () {
+          this.querySelector('img').classList = 'animate-opacity-img';
+          this.querySelector('.description').classList = 'description animate-opacity-text';
+          this.querySelector('p').classList = 'titre animate-opacity-text';
+        };
+        article.onmouseleave = function () {
+          this.querySelector('img').classList = 'animate-opacity-text';
+          this.querySelector('.description').classList = 'description animate-opacity-img';
+          this.querySelector('p').classList = 'titre animate-opacity-img';
+        };
+        article.appendChild(img);
+        article.className = 'taille';
+        article.appendChild(para);
+        article.appendChild(desc);
+        lien.appendChild(article);
+        document.querySelector('#film').appendChild(lien);
+      });
     };
-    xhttp.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=bbad9d275376358a8c117caf83f0cba0&language=fr-FR", true);
-    xhttp.send();
-   
   };
+  xhttp.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=bbad9d275376358a8c117caf83f0cba0&language=fr-FR", true);
+  xhttp.send();
+
+};
 console.log(idDeFilm);
-
-
--- V2/JS/unFilm.js
+++ V2/JS/unFilm.js
@@ -1,45 +1,52 @@
-let params = new URLSearchParams(document.location.search);//document point sur la page, location sur l'url, et search sur ce qu'il y'a apres  le ?
+let params = new URLSearchParams(document.location.search); //document point sur la page, location sur l'url, et search sur ce qu'il y'a apres  le ?
 let name = params.get("idDeFilm");
 console.log(name);
 
-function onclickFilm(id){
+function onclickFilm(id) {
     console.log('salut');
     $.ajax({
         url: `https://api.themoviedb.org/3/movie/${id}?api_key=bbad9d275376358a8c117caf83f0cba0&language=fr-FR`,
-        success: function(result){
+        success: function (result) {
             console.log(result);
             //image
             let img = document.createElement('img');
-            img.className='uneImage';
-            img.src = "https://image.tmdb.org/t/p/w500"+result.poster_path;
-            //image background
-            let imgBack=document.createElement('img');
-            imgBack.className='backImage';
-            imgBack.src = "https://image.tmdb.org/t/p/w500"+result.backdrop_path;
+            img.className = 'uneImage';
+            img.src = "https://image.tmdb.org/t/p/w500" + result.poster_path;
             //texte
             let syno = document.createElement('article');
-            syno.className='synopsis';
+            syno.className = 'synopsis';
             syno.textContent = result.overview;
             //titre
             let titre = document.createElement('h1');
-            titre.className='titre';
-            titre.textContent=result.title;
+            titre.className = 'titre';
+            titre.textContent = result.title;
             $("aside").append(img);
             $("#unFilm").append(titre);
             $("#unFilm").append(syno);
-            /* $("#unFilm").append(imgBack); */
-      }}
-      );
-    };
-function acteur(id){
-    $.ajax({
-        url: `https://api.themoviedb.org/3/movie/${id}/credits?api_key=bbad9d275376358a8c117caf83f0cba0&language=fr-FR`,
-        success: function(credits){
-            console.log(credits.cast);
-
         }
     });
 };
 
-    window.onload =onclickFilm(name);
-    window.onload =acteur(name);
+function acteur(id) {
+    $.ajax({
+        url: `https://api.themoviedb.org/3/movie/${id}/credits?api_key=bbad9d275376358a8c117caf83f0cba0&language=fr-FR`,
+        success: function (credits) {
+            console.log(credits); 
+            var e = credits.cast;
+             console.log(e);
+             var liste = document.createElement('ul');
+            e.forEach(function (element, index) {
+                if (index >= 0 && index <= 5) {
+                    var unActeur=document.createElement('li');
+                    unActeur.textContent = element.name;
+                    liste.appendChild(unActeur);
+                    console.log(element.name);
+                }
+            }); 
+            document.querySelector('#unFilm').appendChild(liste);
+        }
+    });
+};
+
+window.onload = onclickFilm(name);
+window.onload = acteur(name);
